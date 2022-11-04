d0f08c5bdf994143e38b97174c492eba3c9a1545
Pedro Cunha <pedro.cunha@onfinity.com.br>
2022-11-04 19:44:27 -0300

    chore(setup): initial commit

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor='gray.950'>
+            <Text color='gray.200' fontSize='lg'>SignIn</Text>
+        </Center>
+    );
+}
